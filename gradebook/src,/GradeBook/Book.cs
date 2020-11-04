16b4a16b13c8c7035d7a748971e34f431cf871e1
Jacob Gartenstein <jake@Jacobs-MacBook-Pro.local>
2020-11-04 14:27:58 -0600

    Adding Methods

--- gradebook/src,/GradeBook/Book.cs
+++ gradebook/src,/GradeBook/Book.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System;
 
 namespace GradeBook{
 
     class Book{
 
-        List<double> grades;
+        public List<double> grades;
+        private List<string> names;
 
 
         public void AddGrade(double grade){
@@ -12,8 +14,26 @@ namespace GradeBook{
             return;
         }
 
+        public void changeName(List<string> names){
+            this.names = names;
+        }
+
         public Book(){
             this.grades = new List<double>();
+        }
+
+        public void showStatistics(){
+            double max = 0;
+            double min = double.MaxValue;
+
+            foreach(var number in grades){
+                max = Math.Max(number, max);
+                min = Math.Min(number, min);
+            }
+
+            Console.Write($"The highest grade is: {max}\n");
+            Console.Write($"The lowest grade is: {min}\n");
+            
         }
 
     }
--- gradebook/src,/GradeBook/Program.cs
+++ gradebook/src,/GradeBook/Program.cs
@@ -8,36 +8,11 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            double x = 14.1;
-            var y = 47.9075;
-            double z = x + y;
-            Console.WriteLine(z);
+            var book = new Book();
+            book.AddGrade(80);
+            book.AddGrade(91);
 
-            double [] numbers = new double[] {2,4,5,6};
-            numbers[0] = 2;
-            numbers[1] = 4.5;
-            numbers[2] = numbers[0] + numbers[1];
-
-            double result = 0;
-            foreach(double number in numbers){
-                result += number;
-            }
-            Console.WriteLine(result);
-
-
-            List<double> grades = new List<double>();
-            grades.Add(42.9);
-            grades.Add(84.3);
-            grades.Add(91.0);
-            grades.Add(81.1);
-
-            result = 0;
-            foreach (double grade in grades){
-                result += grade;
-            }
-            result /= grades.Count;
-
-
+            book.showStatistics();
            
         }
     }
